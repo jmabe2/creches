94402e74a6be98124d798922f3d7845fa31e93b3
Fouad <Fouad@Fouad-PC>
2017-03-10 19:13:28 +0100

    Modif contact

--- src/com/creche/controller/ContactController.java
+++ src/com/creche/controller/ContactController.java
@@ -6,6 +6,7 @@ import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import javax.persistence.Temporal;
@@ -22,6 +23,9 @@ public class ContactController implements Serializable{
 	private List <Contact> listContact;
 	private Integer contactID;
 	private Contact contact;
+
+	private Boolean visible;
+
 
 	public ContactController(){
 
@@ -44,11 +48,29 @@ public class ContactController implements Serializable{
 		return "ContactForm";
 	}
 
-/*
+	public void showLines(ActionEvent e){
+		visible=true;
+	}
+	
+	public void hide(ActionEvent e){
+		visible=false;
+	}
+	
+	/*
 	 public void removeContact() {
 		ContactService cService = new ContactService();
 		contact = cService.removeContact(contactID);
 	 }*/
+
+	public Integer getContactID() {
+		return contactID;
+	}
+
+	public void setContactID(Integer contactID) {
+		this.contactID = contactID;
+	}
+
+
 
 	public void loadContact(){
 		ContactService cService = new ContactService();
@@ -70,5 +92,17 @@ public class ContactController implements Serializable{
 	public void setContact(Contact contact) {
 		this.contact = contact;
 	}
+
+	public Boolean getVisible() {
+		return visible;
+	}
+
+	public void setVisible(Boolean visible) {
+		this.visible = visible;
+	}
+
+
+
 }
+
 
